4ee9d1dd600139a4935e2a349418b2590d53ba70
Pieter-Jan Noë <pieterjan.noe@external.atlascopco.com>
2024-12-02 18:26:53 +0100

    day 1

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,29 @@
+﻿var textFile = "C:\\Users\\a00579912\\Documents\\AdventOfCode2024\\Day1\\Day1\\input.txt";
+
+List<int> firstRow = new List<int>();
+List<int> secondRow = new List<int>();
+
+using (StreamReader file = new StreamReader(textFile))
+{
+    string ln;
+
+    while ((ln = file.ReadLine()) != null)
+    {
+        var parts = ln.Split("   ");
+        firstRow.Add(int.Parse(parts[0]));
+        secondRow.Add(int.Parse(parts[1]));
+    }
+    file.Close();
+}
+
+firstRow.Sort();
+secondRow.Sort();
+
+var totalDifference = 0;
+for (int i = 0; i <= firstRow.Count-1; i++)
+{
+    totalDifference += Math.Abs(firstRow[i] - secondRow[i]);
+}
+
+Console.WriteLine("Difference: {0}",totalDifference);
+Console.ReadLine();
---
+++ Day1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿var textFile = "C:\\Users\\a00579912\\Documents\\AdventOfCode2024\\Day1\\Day1\\input.txt";
+
+List<int> firstRow = new List<int>();
+List<int> secondRow = new List<int>();
+
+using (StreamReader file = new StreamReader(textFile))
+{
+    string ln;
+
+    while ((ln = file.ReadLine()) != null)
+    {
+        var parts = ln.Split("   ");
+        firstRow.Add(int.Parse(parts[0]));
+        secondRow.Add(int.Parse(parts[1]));
+    }
+    file.Close();
+}
+
+var result = 0;
+for (int i = 0; i <= firstRow.Count - 1; i++)
+{
+    var numberOfAppierences = secondRow.Count(x => x == firstRow[i]);
+    result += firstRow[i] * numberOfAppierences;
+}
+
+Console.WriteLine("Appierences: {0}", result);
+Console.ReadLine();
